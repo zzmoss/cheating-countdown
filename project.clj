5a9dfca80206ecd121c9d7e9ffd4fc4f52ae5ff4
Madhumitha Viswanathan <madhuvishy@gmail.com>
2014-04-04 15:32:09 -0400

    Bare bones lein om template

---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject cheating-countdown "0.1.0-SNAPSHOT"
+  :description "Countdown timer with a cheating mode"
+  :url "http://github.com/madhuvishy/cheating-countdown"
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2156"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [om "0.5.0"]]
+
+  :plugins [[lein-cljsbuild "1.0.2"]]
+
+  :source-paths ["src"]
+
+  :cljsbuild { 
+    :builds [{:id "cheating-countdown"
+              :source-paths ["src"]
+              :compiler {
+                :output-to "cheating_countdown.js"
+                :output-dir "out"
+                :optimizations :none
+                :source-map true}}]})
