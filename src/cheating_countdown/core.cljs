6f8bf7e46233c58d6bb08f45c312a1fe8cd6b36e
Madhumitha Viswanathan <madhuvishy@gmail.com>
2014-04-07 13:55:56 -0400

    Countdown days minutes hour seconds

--- src/cheating_countdown/core.cljs
+++ src/cheating_countdown/core.cljs
@@ -4,18 +4,51 @@
 
 (enable-console-print!)
 
-(def app-state (atom {:count 1}))
+(def ms-in-a-second 1000)
+(def seconds-in-a-minute 60)
+(def seconds-in-an-hour (* 60 seconds-in-a-minute))
+(def seconds-in-a-day (* 24 seconds-in-an-hour))
+
+(def total-time-to-elapse 108440)
+(def days (quot total-time-to-elapse seconds-in-a-day))
+(def hours (quot (rem total-time-to-elapse seconds-in-a-day) seconds-in-an-hour))
+(def minutes (quot (rem total-time-to-elapse seconds-in-an-hour) seconds-in-a-minute))
+(def seconds (rem total-time-to-elapse seconds-in-a-minute))
+
+(println days hours minutes seconds)
+(def app-state (atom  {:days days 
+                       :hours hours 
+                       :minutes minutes 
+                       :seconds seconds}))
+
+(defn display [app]
+  (dom/div nil
+           (dom/ul nil 
+                   (dom/li nil (:days app))
+                   (dom/li nil (:hours app))
+                   (dom/li nil (:minutes app))
+                   (dom/li nil (:seconds app)))))
 
 (defn count-view [app owner]
   (reify
     om/IWillMount
     (will_mount[_]
       (js/setInterval
-        (fn [] (om/transact! app :count inc))
-        1000))
+        (fn [] (om/transact! app :seconds dec))
+        ms-in-a-second)
+      (js/setInterval
+        (fn [] (om/transact! app :minutes dec))
+        (* ms-in-a-second seconds-in-a-minute))
+      (js/setInterval
+        (fn [] (om/transact! app :hours dec))
+        (* ms-in-a-second seconds-in-an-hour))
+      (js/setInterval
+        (fn [] (om/transact! app :days dec))
+        (* ms-in-a-second seconds-in-a-day)))
+      
     om/IRender
     (render[_]
-      (dom/div nil (:count app)))))
+      (display app))))
 
 (om/root count-view app-state
   {:target (. js/document (getElementById "app"))})
