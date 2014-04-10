04fd73c1d7d6a54f7bedb998630c877b366fabf8
Madhumitha Viswanathan <madhuvishy@gmail.com>
2014-04-10 11:25:50 -0400

    Added datepicker jquery plugin

--- src/cheating_countdown/core.cljs
+++ src/cheating_countdown/core.cljs
@@ -1,19 +1,16 @@
 (ns cheating-countdown.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [goog.Uri :as uri]) 
+  (:import [goog Uri]))
 
 (enable-console-print!)
-
-(def ms-in-a-second 1000)
-(def seconds-in-a-minute 60)
-(def seconds-in-an-hour (* 60 seconds-in-a-minute))
-(def seconds-in-a-day (* 24 seconds-in-an-hour))
 
 (defn date-gen 
   ([] (js/Date.))
   ([date] (js/Date. date)))
 
-(def deadline (date-gen "Mon Apr 27 2014 16:05:17 GMT-0400 (EDT)"))
+(def deadline (date-gen (.getParameterValue (Uri. (.-location js/window)) "date")))
 
 (defn compare-dates [date-end date-start]
   (- (.getTime date-end) (.getTime date-start)))
@@ -32,21 +29,54 @@
 
 (def app-state (atom {:remaining nil}))
 
-(defn display [app]
+(defn time-display [app]
   (dom/div nil
     (apply dom/ul nil
       (map dom/li (repeat nil) (date-vec (:remaining app))))))
 
 (defn count-view [app owner]
   (reify
-    om/IDidMount
-    (did-mount [_]
-      (js/setInterval
-        #(om/update! app :remaining(compare-dates deadline (date-gen))) 1000)) 
+    om/IWillMount
+    (will-mount [_]
+      (om/update! app :remaining (compare-dates deadline (date-gen)))
+      (om/set-state! owner :interval
+        (js/setInterval
+          #(om/update! app :remaining (compare-dates deadline (date-gen)))
+          1000))) 
+
+    om/IWillUnmount
+    (will-unmount [_]
+      (js/clearInterval (om/get-state owner :interval)))
 
     om/IRender
     (render[_]
-      (display app))))
+      (time-display app))))
+
+(defn testtest 
+  [dp input]
+  (println dp))
+
+(defn form-display [app]
+  (dom/div nil
+           (dom/input #js {:type "text" 
+                           :id "datetimepicker" 
+                           :ref "datetime" })))
+
+
+(defn datepicker-view [app owner]
+  (reify
+    om/IDidMount
+    (did-mount [_]
+      ( -> (om/get-node owner "datetime")
+           js/jQuery
+           (.datetimepicker (clj->js {:onChangeDateTime testtest}))))
+
+    om/IRender
+    (render [_]
+      (form-display app))))
 
 (om/root count-view app-state
-  {:target (. js/document (getElementById "app"))})
+  {:target (. js/document (getElementById "app1"))})
+
+(om/root datepicker-view app-state
+  {:target (. js/document (getElementById "app0"))})
